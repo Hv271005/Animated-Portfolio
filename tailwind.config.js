76ba32a22066936fb5e5057266be6f782cb123fa
Hv271005 <159716337+Hv271005@users.noreply.github.com>
2024-02-22 14:10:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      zIndex: {
+        '100': '100',
+        '1' : '1',
+        '-1' : '-1',
+      }
+      
+    },
+  },
+  plugins: [],
+}
+
